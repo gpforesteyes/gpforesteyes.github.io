ab3a162ef32cc4977c28077d5f2605d800054f76
lucasbielak <84245400+lucasbielak@users.noreply.github.com>
2025-06-02 12:19:08 +0000

    Deploying to gh-pages from @ gpforesteyes/gpforesteyes.github.io@9746f4e3bd0443ff7cf494253dcea21239ee3428 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-publicações",
           title: "Publicações",
-          description: "Publicações por categoria em ordem cronologica reversa, feita por jekyll-scholar.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
@@ -53,46 +53,36 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-integração-com-técnicas-de-inteligência-artificial-aprendizado-de-máquinas",
-          title: 'Integração com técnicas de inteligência artificial (aprendizado de máquinas)',
-          description: "Aplicação de técnicas de IA e machine learning para análise e gestão florestal",
+          section: "News",},{id: "projects-sistema-de-amostragem-e-inteligência-artificial-para-estimativas-da-biomassa-abaixo-do-solo-bgb",
+          title: 'Sistema de amostragem e Inteligência Artificial para Estimativas da Biomassa Abaixo do Solo...',
+          description: "Below Ground Biomass",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/ai_forest/";
-            },},{id: "projects-avanços-técnicos-e-tecnológicos-para-as-florestas-plantadas-e-nativas",
-          title: 'Avanços técnicos e tecnológicos para as florestas plantadas e nativas',
-          description: "Desenvolvimento e aplicação de novas tecnologias para manejo florestal",
+              window.location.href = "/projects/BGB/";
+            },},{id: "projects-jcarbon-plus",
+          title: 'JCARBON® PLUS',
+          description: "Integração Digital para Monitoramento dos Estoques de Biomassa e Carbono em Florestas Nativas",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/forest_advances/";
-            },},{id: "projects-aplicações-e-integração-com-imageamento-por-sensor-hiperespectral",
-          title: 'Aplicações e integração com Imageamento por Sensor Hiperespectral',
-          description: "Utilização de sensores hiperespectrais para análise detalhada de características florestais",
+              window.location.href = "/projects/Jcarbon/";
+            },},{id: "projects-monitoramento-do-estoque-de-carbono-e-prevenção-de-desastres-naturais-na-serra-do-mar-paraná",
+          title: 'Monitoramento do estoque de carbono e prevenção de desastres naturais na Serra do...',
+          description: "Desenvolvimento e integração de tecnologias",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/hyperspectral_forest/";
-            },},{id: "projects-aplicações-de-imageamento-por-lidar-para-as-estimativas-florestais",
-          title: 'Aplicações de Imageamento por LiDAR para as estimativas Florestais',
+              window.location.href = "/projects/desastres/";
+            },},{id: "projects-integração-de-geotecnologias-para-extração-de-métricas-florestais",
+          title: 'Integração de geotecnologias para extração de métricas florestais',
+          description: "Integração de geotecnologias para extração de métricas florestais",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/integracao/";
+            },},{id: "projects-lid4-estimativas-em-inventários-florestais-nova-era-com-sensores-lidar",
+          title: 'Lid4 - Estimativas em Inventários florestais: Nova era com sensores Lidar',
           description: "Utilização de tecnologia LiDAR para estimativas precisas de atributos florestais",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/lidar_forest/";
-            },},{id: "projects-aplicações-de-sensores-multiespectrais-para-avaliações-florestais",
-          title: 'Aplicações de sensores multiespectrais para avaliações Florestais',
-          description: "Utilização de sensores multiespectrais para monitoramento e avaliação florestal",
+              window.location.href = "/projects/lid4d/";
+            },},{id: "projects-sif4d-inovação-na-fusão-de-dados-para-o-monitoramento-espacial-avançado-da-dinâmica-do-carbono-na-vegetação",
+          title: 'SIF4D – Inovação na fusão de dados para o monitoramento espacial avançado da...',
+          description: "Um enfoque para os plantios florestais brasileiros",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/multispectral_forest/";
-            },},{id: "projects-análises-espaciais-e-geoestatistica-aplicada-as-estimativas-florestais",
-          title: 'Análises espaciais e geoestatistica aplicada as estimativas florestais',
-          description: "Aplicação de técnicas geoestatísticas e análises espaciais para melhorar a precisão das estimativas florestais",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/spatial_analysis/";
-            },},{id: "projects-aplicações-de-sensores-para-propriedades-químicas-físicas-e-morfológicas-das-árvores",
-          title: 'Aplicações de sensores para propriedades químicas, físicas e morfológicas das árvores',
-          description: "Análise avançada de propriedades das árvores utilizando sensores especializados",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/tree_properties/";
-            },},{id: "projects-aquisição-e-imageamento-com-veiculos-aéreos-não-tripulados-vants",
-          title: 'Aquisição e Imageamento com Veiculos Aéreos Não Tripulados - VANTs',
-          description: "Utilização de drones para aquisição de dados florestais",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/uav_forest/";
+              window.location.href = "/projects/sif4d/";
             },},{
         id: 'social-email',
         title: 'email',
